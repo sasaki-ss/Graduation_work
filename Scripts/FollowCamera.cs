aed339ef3fe66438069145bce8590d5608ae307b
澤村遼 <0vs2ky3f1gw5p8c@ezweb.ne.jp>
2021-04-23 08:57:11 +0900

    [update]キャラステータスのスクリプト追加、前のスクリプトにコメント整備

--- Scripts/FollowCamera.cs
+++ Scripts/FollowCamera.cs
@@ -1,21 +1,22 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class FollowCamera : MonoBehaviour
 {
+    //追尾するキャラ
     [SerializeField] Transform player;
-
 
     void Update()
     {
-        // 座標を取得
+        // 座標を取得して移動させる
         Vector3 pos = player.position;
         transform.position = player.position;
+
+        //俯瞰視点にするため少しずらす
         pos.z = transform.position.z - 10;
         pos.y = transform.position.y + 10;
         transform.position = pos;
 
+        //Cameraの角度を傾ける
         transform.rotation = Quaternion.Euler(45,0, 0);
     }
 }
