7fcf70e861f8c91064c627fbc3094c3d0f87cafb
澤村遼 <0vs2ky3f1gw5p8c@ezweb.ne.jp>
2021-04-30 12:43:25 +0900

    [update]移動時の慣性を修正 　　　   自動移動時の処理(製作途中)の追加

--- Scripts/CharacterMove.cs
+++ Scripts/CharacterMove.cs
@@ -8,12 +8,16 @@ public class CharacterMove : MonoBehaviour
 
     //理解はしてないけど3d空間上でのClick座標を取得するのに使う
     RaycastHit hit;
-
+    int a = 0;
     //キャラ
     [SerializeField] Animator animator;
     [SerializeField] Transform player;
+    [SerializeField] GameObject ball;
 
-    Vector3 a;
+    //前の座標と今の座標を比べるために使う変数
+    Vector3 nowPosition;
+
+    //タッチ時に使う処理
     Vector2 touch;
     Vector3 touchPosition;
 
@@ -24,6 +28,7 @@ public class CharacterMove : MonoBehaviour
 
     void Update()
     {
+        /*製作途中
         if (Input.GetKey("right"))
         {
             animator.SetBool("is_RightShake", true);
@@ -34,8 +39,23 @@ public class CharacterMove : MonoBehaviour
         }
         if (Input.GetKey("down"))
         {
+           // AutoMove();
             animator.SetBool("is_Volley", true);
         }
+
+        if(animator.GetBool("is_Volley")==true)
+        {
+            Debug.Log(a);
+            a++;
+
+            if(a>100)
+            {
+                a = 0;
+                //プレイヤーを立ち止まりモーションにする
+                animator.SetBool("is_Volley", false);
+            }
+        }
+        */
         /*
         //タッチ時の処理　確認はしてない
         if (Input.GetTouch(0).phase == TouchPhase.Moved)
@@ -45,16 +65,54 @@ public class CharacterMove : MonoBehaviour
             touchPosition = new Vector3(touch.x, touch.y,0);
         }
         */
+
         //左クリックしたら
         if (Input.GetMouseButtonDown(0))
         {
+            //現状の移動指定地を削除
+            GetComponent<NavMeshAgent>().ResetPath();
+
+            //クリック時の処理
             MoveToCursor();
         }
 
-        //現座標と前座標に違う場合
-        if (player.position != a)
+        //オート移動処理
+        /*if(プレイヤーに対して向かってきている状態の弾なら)
         {
+            AutoMove();
+        }
+        */
+
+        //座標判定
+        PositionJudge();
+       
+    }
+
+    private void MoveToCursor()
+    {
+        //カメラの場所とマウスのClick座標からZ座標を求める
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+        //タッチ時の処理　確認はしてない
+        //Ray ray = Camera.main.ScreenPointToRay(touchPosition);
+
+        bool hasHit = Physics.Raycast(ray, out hit);
+
+        //Clickした場所まで移動
+        if (hasHit)
+        {
+            GetComponent<NavMeshAgent>().destination = hit.point;
+        }
+    }
+
+    private void PositionJudge()
+    {
+        //現座標と前座標に違う場合
+        if (player.position != nowPosition)
+        {
+            //プレイヤーを走るモーションにする
             animator.SetBool("is_Run", true);
+
             //移動中ならスタミナ減少(今いる座標と目的地の座標のズレが1以上の場合)
             if (hit.point.x - player.position.x < -1)
             {
@@ -88,25 +146,21 @@ public class CharacterMove : MonoBehaviour
         }
         else
         {
+            //プレイヤーを立ち止まりモーションにする
             animator.SetBool("is_Run", false);
         }
-        a = player.position;
+
+        //現在の座標を取得
+        nowPosition = player.position;
     }
 
-    private void MoveToCursor()
+    private void AutoMove()
     {
-        //カメラの場所とマウスのClick座標からZ座標を求める
-        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        //ボールのオブジェクトの情報を取得
+        ball = GameObject.Find("ball");
 
-        //タッチ時の処理　確認はしてない
-        //Ray ray = Camera.main.ScreenPointToRay(touchPosition);
-
-        bool hasHit = Physics.Raycast(ray, out hit);
-
-        //Clickした場所まで移動
-        if (hasHit)
-        {
-            GetComponent<NavMeshAgent>().destination = hit.point;
-        }
+        //移動させる
+        GetComponent<NavMeshAgent>().destination = ball.transform.position;      
+        //Debug.Log(ball.transform.position);
     }
 }
